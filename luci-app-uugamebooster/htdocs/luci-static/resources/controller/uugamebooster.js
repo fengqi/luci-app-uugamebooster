cfd1f5fd85bd5bee1b50427c2fe6bc8ccb338425
fengqi <lyf362345@gmail.com>
2026-06-21 04:15:46 +0800

    luci-app-uugamebooster: 修复菜单入口不显示的问题
    fs.stat() 需要 ubus file 权限，ACL 未授权导致菜单注册失败。
    移除文件检查，因为 uugamebooster 包必定安装配置文件。

--- luci-app-uugamebooster/htdocs/luci-static/resources/controller/uugamebooster.js
+++ luci-app-uugamebooster/htdocs/luci-static/resources/controller/uugamebooster.js
@@ -1,17 +1,14 @@
 'use strict';
 'require controller';
-'require fs';
 
 return controller.extend({
 	index: function() {
-		return fs.stat('/etc/config/uugamebooster').then(function() {
-			controller.add({
-				path: ['admin', 'services', 'uugamebooster'],
-				title: _('UU GameAcc'),
-				order: 99,
-				action: 'uugamebooster/uugamebooster',
-				acl: ['luci-app-uugamebooster']
-			});
-		}).catch(function() {});
+		controller.add({
+			path: ['admin', 'services', 'uugamebooster'],
+			title: _('UU GameAcc'),
+			order: 99,
+			action: 'uugamebooster/uugamebooster',
+			acl: ['luci-app-uugamebooster']
+		});
 	}
 });
